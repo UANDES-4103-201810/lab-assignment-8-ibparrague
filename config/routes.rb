7432e4dc5af30afcf6346bf721a5491c818cf074
gusvicuna <gpvicuna@miuandes.cl>
2018-05-08 16:52:58 -0300

    Added root

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/admin/:id/products', to: 'users#admin_products'
   get '/admin/:id/users/:id/products', to: 'users#admin_user_product'
 
+  root 'users#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
